e34a990364ca3ca777f163a30f0b3306c0d0527c
Guilherme M. Miranda <alchemist.software@proton.me>
2023-12-23 06:25:01 +0000

    Renamed files to their correct names.

---
+++ src/wad-finder.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+dir_doom="${XDG_DATA_HOME:="$HOME/.local/share"}"
+dir_iwad="$dir_doom/iwad"
+dir_ewad="$dir_doom/ewad"
+
+iwad_doom_2="$dir_iwad/doom2.wad"
+iwad_doom_1="$dir_iwad/doom1.wad"
+iwad_doom_ultimate="$dir_iwad/doomu.wad"
+
+ewad_nerve="$dir_iwad/nerve.wad"
+ewad_sigil_1="$dir_pwad/sigil1.wad"
+ewad_sigil_2="$dir_pwad/sigil2.wad"
+
+function main {
+  return 0
+}
+
