dd4ddbc309eba3b258ae5a0a7cc8c39de9b91dd6
Guilherme M. Miranda <alchemist.software@proton.me>
2023-12-24 00:29:23 +0000

    feat: Added Steam directory validation.
    Added new functions to validate existence of  folder
    and Doom 1 and Doom 2 installation.

--- src/wad-finder.sh
+++ src/wad-finder.sh
@@ -1,20 +1,67 @@
 #!/usr/bin/env bash
 
+# Bash's unofficial strict mode
 set -euo pipefail
 
-dir_doom="${XDG_DATA_HOME:="$HOME/.local/share"}"
-dir_iwad="$dir_doom/iwad"
-dir_ewad="$dir_doom/ewad"
+dir_data="${XDG_DATA_HOME:="$HOME/.local/share"}"
 
-iwad_doom_2="$dir_iwad/doom2.wad"
-iwad_doom_1="$dir_iwad/doom1.wad"
-iwad_doom_ultimate="$dir_iwad/doomu.wad"
+dir_main="$dir_data/wad-finder"
 
-ewad_nerve="$dir_iwad/nerve.wad"
-ewad_sigil_1="$dir_pwad/sigil1.wad"
-ewad_sigil_2="$dir_pwad/sigil2.wad"
+dir_iwad="$dir_data/wad-finder/doom-iwad"
+dir_ewad="$dir_data/wad-finder/doom-ewad"
 
-function main {
-  return 0
+iwad_doom_2="$dir_data/wad-finder/doom-iwad/doom2.wad"
+iwad_doom_1="$dir_data/wad-finder/doom-iwad/doom1.wad"
+iwad_doom_u="$dir_data/wad-finder/doom-iwad/doomu.wad"
+
+ewad_nerve="$dir_data/wad-finder/doom-ewad/nerve.wad"
+ewad_sigil_1="$dir_data/wad-finder/doom-ewad/sigil1.wad"
+ewad_sigil_2="$dir_data/wad-finder/doom-ewad/sigil2.wad"
+
+check_steam_directory() {
+    echo "[ Info ]: Checking Steam directory."
+
+    if ! [ -d "$dir_data/Steam" ]; then
+	echo "[ Error ]: Steam directory does exit, have you installed it on the $XDG_DATA_HOME?"
+	exit 1
+    fi
+
+    echo "[ Success ]: Steam directory found."
+    return 0
 }
+
+check_doom_1_directory() {
+    echo "[ Info ]: Checking Doom installation."
+
+    if ! [ -d "$dir_data/Steam/steamapps/common/Ultimate Doom" ]; then
+	echo "[ Error ]: Could not find Ultimate Doom intallation."
+	echo "[ Error ]: Check your Ultimate Doom installation on the Steam Client."
+	echo "[ Error ]: Make sure to install in your \$XDG_DATA_HOME"
+    fi
+
+    echo "[ Success ]: Ultimate Doom steam installation found."
+    return 0
+}
+
+check_doom_2_directory() {
+    echo "[ Info ]: Checking Doom 2 installation."
+
+    if ! [ -d "$dir_data/Steam/steamapps/common/Doom 2" ]; then
+	echo "[ Error ]: Could not find Doom 2 intallation."
+	echo "[ Error ]: Check your Doom 2 installation on the Steam Client."
+	echo "[ Error ]: Make sure to install in your \$XDG_DATA_HOME"
+    fi
+
+    echo "[ Success ]: Doom 2 steam installation found."
+    return 0
+}
+
+main() {
+    check_steam_directory
+    check_doom_1_directory
+    check_doom_2_directory
+    return 0
+}
+
+main "$@"
 
