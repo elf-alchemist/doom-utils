a93a274ac726ce10d6729b597859d6d466280ce4
Guilherme M. Miranda <alchemist.software@proton.me>
2023-12-25 03:41:46 +0000

    chore: Added relevent SHA1 checksum values.

--- src/wad-finder.sh
+++ src/wad-finder.sh
@@ -12,11 +12,17 @@ dir_ewad="$dir_data/wad-finder/doom-ewad"
 
 iwad_doom_2="$dir_data/wad-finder/doom-iwad/doom2.wad"
 iwad_doom_1="$dir_data/wad-finder/doom-iwad/doom1.wad"
-iwad_doom_u="$dir_data/wad-finder/doom-iwad/doomu.wad"
 
 ewad_nerve="$dir_data/wad-finder/doom-ewad/nerve.wad"
 ewad_sigil_1="$dir_data/wad-finder/doom-ewad/sigil1.wad"
 ewad_sigil_2="$dir_data/wad-finder/doom-ewad/sigil2.wad"
+
+iwad_doom_2_sha1="7ec7652fcfce8ddc6e801839291f0e28ef1d5ae7"
+iwad_doom_1_sha1="9b07b02ab3c275a6a7570c3f73cc20d63a0e3833"
+
+ewad_nerve_sha1="3451288383fb16e196f273d9f85d58c1fda97bf4"
+ewad_sigil_2_sha1="ad2c6e8367afbeef74e9e09b6b1e4da88c0576b4"
+ewad_sigil_1_sha1="e2efdf379e1383c4e15c03de89063361897cd459"
 
 check_steam_directory() {
     echo "[ Info ]: Checking Steam directory."
@@ -60,6 +66,7 @@ main() {
     check_steam_directory
     check_doom_1_directory
     check_doom_2_directory
+
     return 0
 }
 
