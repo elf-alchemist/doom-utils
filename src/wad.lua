6f3d9b4ff9cbffd73e0499dbad3796331f6a5b6d
Guilherme M. Miranda <alchemist.software@proton.me>
2024-01-06 02:48:27 +0000

    feat: added help and default commands

--- src/wad.lua
+++ src/wad.lua
@@ -1,21 +1,33 @@
-local find = function()
+local function find()
 	local home = os.getenv('HOME')
 	local data_dir = os.getenv('XDG_DATA_HOME') or (home .. '/.local/share')
 end
 
+local function help()
+	print(
+	'Usage:\n' ..
+	'    wad [-v|--version]\n' ..
+	'    wad <command> [<subcommand>] [-h|--help]\n' ..
+	'\n' ..
+	'Managing DooM wads in POSIX environments\n' ..
+	'\n' ..
+	'For more information, visit https://github.com/elf-alchemist/doom-utils')
+end
+
 local command_list = {
-	find = find
+	find = find,
+	help = help,
 }
 
-local main = function ()
-	local cmd = arg[2]
+local function main()
+	local cmd = arg[1]
 
 	local func = command_list[cmd]
 
 	if (func ~= nil) then
 		func()
 	else
-		io.stderr('Command not found:' .. cmd)
+		io.stderr:write('Command not found: \'' .. cmd .. '\' try \'wad --help\'\n')
 	end
 end
 
