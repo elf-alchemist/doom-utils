df1645e89512e121daa2533e0d505456042ac39e
Guilherme M. Miranda <alchemist.software@proton.me>
2024-01-06 16:32:55 +0000

    feat: added funtional find command

--- src/wad.lua
+++ src/wad.lua
@@ -1,3 +1,24 @@
+local function fileExists(path)
+    local file = io.open(path, "r")
+
+	if file then
+        file:close()
+        return true
+    else
+        return false
+    end
+end
+
+local function moveFile(src, dest)
+    local status, result, code = os.execute('cp "' .. src .. '" "' .. dest .. '"')
+
+	if not status then
+        print('Error: ' .. result .. ':' .. code)
+    else
+        print('File ' .. src .. ' moved successfully.')
+    end
+end
+
 local function find()
 	local home      = os.getenv('HOME')
 	local data_dir  = os.getenv('XDG_DATA_HOME') or (home .. '/.local/share')
@@ -6,6 +27,7 @@ local function find()
 	local steam_path = {
 		doom1 = steam_dir .. '/Ultimate Doom/base/DOOM.WAD',
 		doom2 = steam_dir .. '/Doom 2/base/DOOM2.WAD',
+		nrftl = nil,
 	}
 
 	local dir = {
@@ -15,15 +37,12 @@ local function find()
 		pwad = data_dir .. '/doom/pwad',
 	}
 
-	local iwad = {
-		doom1 = dir['iwad'] .. '/doom1.wad',
-		doom2 = dir['iwad'] .. '/doom2.wad',
-	}
-
-	local ewad = {
-		sigil1 = dir['ewad'] .. '/sigil1.wad',
-		sigil2 = dir['ewad'] .. '/sigil2.wad',
-		nrftl  = dir['ewad'] .. '/nerve.wad',
+	local wad = {
+		doom1  = dir.iwad .. '/doom1.wad',
+		doom2  = dir.iwad .. '/doom2.wad',
+		sigil1 = dir.ewad .. '/sigil1.wad',
+		sigil2 = dir.ewad .. '/sigil2.wad',
+		nrftl  = dir.ewad .. '/nrftl.wad',
 	}
 
 	local sha1 = {
@@ -33,6 +52,32 @@ local function find()
 		sigil2 = 'ad2c6e8367afbeef74e9e09b6b1e4da88c0576b4',
 		nrftl  = '3451288383fb16e196f273d9f85d58c1fda97bf4',
 	}
+
+	if not fileExists(dir.main) then
+		for _, path in pairs(dir) do
+			local success, result, code = os.execute('mkdir -p ' .. path)
+
+			if success then
+				print('Successfully created directory')
+			else
+				print('Could not crete directory: ' .. path)
+				print('Recieved result of ' .. result .. ':' .. code)
+			end
+		end
+	end
+
+	for name, path in pairs(steam_path) do
+		local localPath       = wad[name]
+		local steamFileExists = fileExists(path)
+
+		if not steamFileExists then
+			print('Error: file for \'' .. name .. '\' was not found.')
+			print('Please check your Steam installation of \'' .. name .. '\'')
+			print('Check the path \'' .. path .. '\'')
+		else
+			moveFile(path, localPath)
+		end
+	end
 end
 
 local function help()
