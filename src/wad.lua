8310d78138e5771cf8206e51318218832aa7664c
Guilherme M. Miranda <alchemist.software@proton.me>
2024-01-08 01:31:28 +0000

    style: fixed formatting

--- src/wad.lua
+++ src/wad.lua
@@ -2,31 +2,31 @@
 
 ---@param path string
 local function fileExists(path)
-    local file = io.open(path, "r")
+	local file = io.open(path, "r")
 
 	if file then
-        file:close()
-        return true
-    else
-        return false
-    end
+		file:close()
+		return true
+	else
+		return false
+	end
 end
 
 ---@param src  string
 ---@param dest string
 local function moveFile(src, dest)
-    local status, result, code = os.execute(string.format("cp '%s' '%s'", src, dest))
+	local status, result, code = os.execute(string.format("cp '%s' '%s'", src, dest))
 
 	if not status then
-        io.stdout:write(string.format("Error:%s->%s", result, code))
-    else
-        io.stdout:write(string.format("File %s moved successfully.", src))
-    end
+		io.stdout:write(string.format("Error:%s->%s", result, code))
+	else
+		io.stdout:write(string.format("File %s moved successfully.", src))
+	end
 end
 
 local function find()
-	local home      = os.getenv("HOME")
-	local data_dir  = os.getenv("XDG_DATA_HOME") or (home .. "/.local/share")
+	local home = os.getenv("HOME")
+	local data_dir = os.getenv("XDG_DATA_HOME") or (home .. "/.local/share")
 	local steam_dir = data_dir .. "/Steam/steamapps/common"
 
 	local steam_path = {
@@ -36,26 +36,26 @@ local function find()
 	}
 
 	local dir = {
-		main      = data_dir .. "/doom-utils",
+		main = data_dir .. "/doom-utils",
 		doom_iwad = data_dir .. "/doom-utils/doom-iwad",
 		doom_ewad = data_dir .. "/doom-utils/doom-ewad",
 		doom_pwad = data_dir .. "/doom-utils/doom-pwad",
 	}
 
 	local wad = {
-		doom1  = dir.doom_iwad .. "/doom1.wad",
-		doom2  = dir.doom_iwad .. "/doom2.wad",
+		doom1 = dir.doom_iwad .. "/doom1.wad",
+		doom2 = dir.doom_iwad .. "/doom2.wad",
 		sigil1 = dir.doom_ewad .. "/sigil1.wad",
 		sigil2 = dir.doom_ewad .. "/sigil2.wad",
-		nrftl  = dir.doom_ewad .. "/nrftl.wad",
+		nrftl = dir.doom_ewad .. "/nrftl.wad",
 	}
 
 	local sha1 = {
-		doom1  = "9b07b02ab3c275a6a7570c3f73cc20d63a0e3833",
-		doom2  = "7ec7652fcfce8ddc6e801839291f0e28ef1d5ae7",
+		doom1 = "9b07b02ab3c275a6a7570c3f73cc20d63a0e3833",
+		doom2 = "7ec7652fcfce8ddc6e801839291f0e28ef1d5ae7",
 		sigil1 = "e2efdf379e1383c4e15c03de89063361897cd459",
 		sigil2 = "ad2c6e8367afbeef74e9e09b6b1e4da88c0576b4",
-		nrftl  = "3451288383fb16e196f273d9f85d58c1fda97bf4",
+		nrftl = "3451288383fb16e196f273d9f85d58c1fda97bf4",
 	}
 
 	if not fileExists(dir.main) then
@@ -74,7 +74,7 @@ local function find()
 	end
 
 	for name, path in pairs(steam_path) do
-		local localPath       = wad[name]
+		local localPath = wad[name]
 		local steamFileExists = fileExists(path)
 
 		if not steamFileExists then
@@ -113,13 +113,12 @@ local function version()
 end
 
 local command_list = {
-	find    = find,
-	help    = help,
+	find = find,
+	help = help,
 	version = version,
 }
 
 local function main()
-
 	for i = 0, #arg do
 		if arg[i] == "--help" then
 			help()
@@ -135,12 +134,9 @@ local function main()
 		func()
 		os.exit(0)
 	else
-		io.stderr:write(
-			string.format("Command not found: '%s' try 'wad help'\n", cmd)
-		)
+		io.stderr:write(string.format("Command not found: '%s' try 'wad help'\n", cmd))
 		os.exit(1)
 	end
 end
 
 main()
-
